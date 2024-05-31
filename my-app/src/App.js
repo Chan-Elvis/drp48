8a42745c2336d5168d9f8a9362eaf1302dcb238e
MSK252 <mk2520@ic.ac.uk>
2024-05-31 05:11:06 +0100

    Implemented barebones page for goal page.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Trip To Italy</h1>
+        <p>23 users saving for this goal</p>
+        <p>64 users achieved this goal</p>
+        <p>Users expected to achieve within a similar timeframe as you:</p>
+        <div>
+          <p>George <button>Follow</button></p>
+          <p>Ivy <button>Follow</button></p>
+        </div>
+        <h2>Featured Stories & Tips</h2>
+        <div>
+          <p>Tim</p>
+          <p>Buy round-trip tickets as soon as you know the starting date of your trip. Always keep track of the ticket price!</p>
+        </div>
+        <div>
+          <p>Olivia</p>
+          <p>If you are going to Firenze, try Trattoria Dall'oste!! It's so delicious, definitely worth the price.</p>
+        </div>
+        <a href="#">Read More Stories & Tips</a>
+        <h2>Memory Collection</h2>
+        <div>
+          <p>John</p>
+          <p>Freya</p>
+          <p>Isabella</p>
+        </div>
       </header>
     </div>
   );
